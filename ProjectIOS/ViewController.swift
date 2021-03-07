0febe874ab7b4c81197f53e15466f4261800de3d
BDA7 <bondare-danila2017@yandex.ru>
2021-03-07 20:47:59 +0300

    Initial Commit

---
+++ ProjectIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectIOS
+//
+//  Created by Александр Хижко on 07.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
