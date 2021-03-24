5abd8328cf0d626c807ac1fcc8441cd396f07db4
BDA7 <bondare-danila2017@yandex.ru>
2021-03-24 21:25:42 +0300

    100%

--- ProjectIOS/ViewController.swift
+++ ProjectIOS/ViewController.swift
@@ -32,9 +32,9 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: UIScreen.main.bounds.width/2, height: UIScreen.main.bounds.height/2)
-    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/3)
+//    }
     
     
     
